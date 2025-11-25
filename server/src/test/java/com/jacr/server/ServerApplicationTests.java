7678e3027359515008b036a7f1dacbdf9697255a
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2025-11-25 16:14:29 -0500

    first commit

---
+++ server/src/test/java/com/jacr/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacr.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
